16d97b4616f146f7885faf89d44f863f8c38d150
Leyla Uzhakhova <luzhakh@gmail.com>
2023-10-16 12:49:35 +0200

    LP-1 - Change Zero on the timer

--- dist/js/script.js
+++ dist/js/script.js
@@ -42,11 +42,16 @@ window.addEventListener('DOMContentLoaded', () => {
   //timer
   const deadline = '2023-10-17';
   function getTimeRemaining(endtime) {
-    const t = Date.parse(endtime) - Date.parse(new Date()),
-      days = Math.floor(t / (1000 * 60 * 60 * 24)),
-      hours = Math.floor(t / (1000 * 60 * 60) % 24),
-      minutes = Math.floor(t / (1000 / 60) % 60),
-      seconds = Math.floor(t / 1000 % 60);
+    let days, hours, minutes, seconds;
+    const t = Date.parse(endtime) - Date.parse(new Date());
+    if (t <= 0) {
+      days = 0;
+      hours = 0;
+      minutes = 0;
+      seconds = 0;
+    } else {
+      days = Math.floor(t / (1000 * 60 * 60 * 24)), hours = Math.floor(t / (1000 * 60 * 60) % 24), minutes = Math.floor(t / (1000 / 60) % 60), seconds = Math.floor(t / 1000 % 60);
+    }
     return {
       'total': t,
       'days': days,
